b7b3d150195ff55673ff94a35fe1b07bb482d669
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-11-13 12:50:03 -0500

    minimal repro

--- index.js
+++ index.js
@@ -21,20 +21,7 @@ connection.connect((err, conn) => {
         console.log('Successfully connected to Snowflake.');
         // Execute a sample query
         const query = `
-        SELECT CURRENT_VERSION();
-        WITH RECURSIVE generate_series(n) AS (
-            SELECT 1
-            UNION ALL
-            SELECT n + 1
-            FROM generate_series
-            WHERE n < 3000000
-        )
-        SELECT 
-            n AS id,
-            RANDSTR(10, RANDOM()) AS random_string,
-            UNIFORM(1, 100, RANDOM()) AS random_number,
-            DATEADD(day, UNIFORM(0, 3650, RANDOM()), CURRENT_DATE()) AS random_date
-        FROM generate_series
+        SELECT 1
         `;
         
         connection.execute({
